cbc153ac1a7e7ee2ed40611f99ab76577dc93032
Eric Milgram <4348294+ScientificProgrammer@users.noreply.github.com>
2021-12-08 14:11:43 -0600

    Make minor formatting changes to the SQL

--- code/SQL/900-010 Examples - Generate UUIDs using PostgreSQL.sql
+++ code/SQL/900-010 Examples - Generate UUIDs using PostgreSQL.sql
@@ -1,6 +1,7 @@
-WITH x AS (
-	SELECT n FROM (VALUES (0),(1),(2),(3),(4),(5),(6),(7),(8),(9)) v(n)
+WITH f AS (
+	SELECT n
+	FROM (VALUES (0),(1),(2),(3),(4),(5),(6),(7),(8),(9)) v(n)
 )
-SELECT ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS rownum, gen_random_uuid() as uuid
-FROM x ones, x tens, x hundreds
+SELECT ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS rownum, gen_random_uuid() AS uuid
+FROM f ones, f tens, f hundreds
 ORDER BY 1;

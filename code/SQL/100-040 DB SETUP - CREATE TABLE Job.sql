f397396d6036686877245960c06e5c7a4e5c0fbd
Eric Milgram <4348294+ScientificProgrammer@users.noreply.github.com>
2021-12-09 12:23:41 -0600

    Update UNIQUE constraint to include FKs for COMPANY, POSITION and EMPLOYEE

--- code/SQL/100-040 DB SETUP - CREATE TABLE Job.sql
+++ code/SQL/100-040 DB SETUP - CREATE TABLE Job.sql
@@ -5,12 +5,16 @@
 
    CREATE TABLE IF NOT EXISTS paylocity.dev.Job
    (
-       guid          uuid   PRIMARY KEY,
-       company_guid  uuid   NOT NULL,
-       position_guid uuid   NOT NULL,
-       employee_guid uuid   NOT NULL,
-       FOREIGN KEY (company_guid)  REFERENCES paylocity.dev.Company  (guid),
-       FOREIGN KEY (position_guid) REFERENCES paylocity.dev.Position (guid),
-       FOREIGN KEY (employee_guid) REFERENCES paylocity.dev.Employee (guid),
-       UNIQUE (company_guid, employee_guid)
+       guid              uuid     NOT NULL,
+       company_guid      uuid     NOT NULL,
+       position_guid     uuid     NOT NULL,
+       employee_guid     uuid     NOT NULL,
+       CONSTRAINT Job_PK                  PRIMARY KEY (guid),
+       CONSTRAINT Job_FK_company_guid     FOREIGN KEY (company_guid)  REFERENCES paylocity.dev.Company  (guid),
+       CONSTRAINT Job_FK_position_guid    FOREIGN KEY (position_guid) REFERENCES paylocity.dev.Position (guid),
+       CONSTRAINT Job_FK_employee_guid    FOREIGN KEY (employee_guid) REFERENCES paylocity.dev.Employee (guid),
+       CONSTRAINT Job_unique_comp_pos_emp UNIQUE      (company_guid, position_guid, employee_guid)
    );
+   
+   COMMENT ON CONSTRAINT Job_unique_comp_pos_emp ON paylocity.dev.Job
+   IS 'Prevent same person from having same job at same company';

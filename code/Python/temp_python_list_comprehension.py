22b9e6c516903e13c80c1e54ee4fb92a19bf524a
Eric Milgram <4348294+ScientificProgrammer@users.noreply.github.com>
2022-01-11 04:06:45 -0600

    Use git rm to remove unncessary files
         pick d01fcd2 Delete unnecessary files and fix CSS bleedthrough
     	deleted:    code/Python/temp_demonstrate_jedi.py
     	deleted:    code/Python/temp_python_list_comprehension.py
     	deleted:    code/Python/temp_python_test_fstrings.py
     	deleted:    code/Python/temp_python_test_print.py

--- code/Python/temp_python_list_comprehension.py
+++
@@ -1,7 +0,0 @@
-""" Demonstrating Python's list comprehension capabilities."""
-
-""" Create a list with the integers 5, 10, 15, 20, 25. """
-p = [5 + x for x in range(5)]
-
-""" Print the Output: [5, 10, 15, 20, 25] """
-print(p)
